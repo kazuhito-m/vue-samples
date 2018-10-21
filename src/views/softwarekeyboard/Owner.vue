bb7598729f41a8a6fdcb0b91a96c3d31e19c9213
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-10-22 00:41:38 +0900

    最低限のサンプルページへの遷移を作成。

---
+++ src/views/softwarekeyboard/Owner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>SoftwareKeyboard and Structure Sample</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component
+export default class Owner extends Vue {}
+</script>
